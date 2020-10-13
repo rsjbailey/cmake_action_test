54b4e057cae186e9a4778c96c01e59f277b2d127
Richard Bailey <rsjtaylor.github@gmail.com>
2020-10-13 14:58:21 +0100

    fix add_path

--- add_path.cmake
+++ add_path.cmake
@@ -1,12 +1,14 @@
-# convert existing env file to cmake list
-file(STRINGS "$ENV{GITHUB_ENV}" env_variables)
+# convert existing path file to cmake list
+file(STRINGS "$ENV{GITHUB_PATH}" path)
 # skip first 4 arguments, i.e.
 # cmake -P set_env.cmake -- 
 set(ARG_NUM 4)
 # iterate over remaining args
 while(ARG_NUM LESS CMAKE_ARGC)
-    list(APPEND env_variables ${CMAKE_ARGV${ARG_NUM}})
+    file(TO_CMAKE_PATH "${CMAKE_ARGV${ARG_NUM}}"} current_path)
+    file(TO_NATIVE_PATH "${added_path}" current_path)
+    list(APPEND path "${current_path}")
     math(EXPR ARG_NUM "${ARG_NUM}+1")
 endwhile()
-list(JOIN env_variables "\n" env_variables)
-file(WRITE "$ENV{GITHUB_ENV}" "${env_variables}")
+list(JOIN path "\n" path)
+file(WRITE "$ENV{GITHUB_PATH}" "${path}")
